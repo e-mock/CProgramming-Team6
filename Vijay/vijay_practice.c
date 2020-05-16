9efc16e094580c3caa7f986e3d0793a31f7baa72
20073046-Onsite <20073046-Onsite@MYTSL00307>
2020-05-16 14:51:02 -0500

    function added

--- Vijay/vijay_practice.c
+++ Vijay/vijay_practice.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+int max_value();
+
 int hello_v() {
    printf("Welcome Team6.!");
-   
+   max_value();
+
    return 0;
+}
+
+/* function to find the maximum value from a list of number*/
+int max_value()
+{
+
+int list[10];
+int max_value = 0;
+
+printf("Enter the list of 10 numbers");
+
+for(int i=0;i<10;i++)
+{
+	scanf("%d", list[i]);
+}
+
+for(int j=0;j<10;j++)
+{
+	if(max_value < list[j])
+	{
+		max_value = list[j];
+	}
+}
+
+printf("The Maximum value in the list is %d",max_value);
+
+return max_value;
+
 }
