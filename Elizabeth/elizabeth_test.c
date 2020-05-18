2c25b886b0038a9ada5d4dde381c967b16bb0a7b
e-mock <lizmock02@gmail.com>
2020-05-17 19:32:15 -0500

    Update elizabeth_test.c
    added more tests

--- Elizabeth/elizabeth_test.c
+++ Elizabeth/elizabeth_test.c
@@ -28,12 +28,42 @@ void test_add(void)
     CU_ASSERT(sum(-1, 1) == 0);
 }
 
-void test_temp_sensor_to_c(void){
+void test_temp_c_to_f(void){
+    //                     temp C     tempF
+    CU_ASSERT(temp_c_to_f(-40.0) == -40.0);
+    CU_ASSERT(temp_c_to_f(0.0) == 32.0);    
+    CU_ASSERT(temp_c_to_f(25.0) == 77.0);
+    CU_ASSERT(temp_c_to_f(100.0) == 212.0);
+}
+
+void test_temp_sensor_to_c_valid(void){
     bool success = false;
     
-    CU_ASSERT(temp_sensor_converter(0, &success), -55.0);
-    CU_ASSERT(temp_sensor_converter(0, &success), -55);
-    CU_ASSERT(temp_sensor_converter(1023, &success), 125.0);
+    CU_ASSERT(temp_sensor_converter(0, &success) == -55.0);
+    CU_ASSERT(success, true);    
+    
+    CU_ASSERT(temp_sensor_converter(511, &success) == 34.912);
+    CU_ASSERT(success, true);
+    
+    CU_ASSERT(temp_sensor_converter(1023, &success) == 125.0);
+    CU_ASSERT(success, true);
+    
+}
+
+void test_temp_sensor_to_c_invalid(void){
+    bool success = false;
+    
+    CU_ASSERT(temp_sensor_converter(-100, &success) == -100.0);
+    CU_ASSERT(success, false); 
+    
+    CU_ASSERT(temp_sensor_converter(-1, &success) == -100.0);
+    CU_ASSERT(success, false);    
+    
+    CU_ASSERT(temp_sensor_converter(1024, &success) == -100.0);
+    CU_ASSERT(success, false);
+    
+    CU_ASSERT(temp_sensor_converter(1100, &success) == -100.0);
+    CU_ASSERT(success, false);   
     
 }
 
@@ -42,11 +72,17 @@ int main() {
     CU_initialize_registry();
 
     CU_pSuite suite = CU_add_suite("test_add", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_temp_c_to_f", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_temp_sensor_to_c_valid", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_temp_sensor_to_c_invalid", 0, 0);
 
 
 
     CU_add_test(suite, "test_add", test_add);
-
+    CU_add_test(suite, "test_temp_c_to_f", test_temp_c_to_f);
+    CU_add_test(suite, "test_temp_sensor_to_c_valid", test_temp_sensor_to_c_valid);
+    CU_add_test(suite, "test_temp_sensor_to_c_invalid", test_temp_sensor_to_c_invalid);
+    
 
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
