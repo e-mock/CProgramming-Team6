d413ed44628b857dcedb226194d7b017517e329f
chandanraoramesh <65356528+chandanraoramesh@users.noreply.github.com>
2020-05-19 18:43:48 -0500

    Update chandan_practice.c

--- Chandan/chandan_practice.c
+++ Chandan/chandan_practice.c
@@ -1,106 +1,132 @@
-#include <stdio.h>
-#include <time.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-#define SECONDS_PER_MINUTE 60
-#define MINUTES_PER_HOUR 60
-#define HOURS_PER_DAY 24
-#define PROMPT_STRING "Type any key to show time, q to show time then quit: "
-
-#define TIME_TYPE unsigned long 
-
-char getChar() ;
-void updateTimes() ;
-void initTimes() ;
-void printPrompt() ;
-void printTime (TIME_TYPE timeInit) ;
-
-TIME_TYPE start, end;
-
-int hello_c()
+int main()
 {
-        char command = 0;
-        initTimes();
-        delay();
-        updateTimes();
-        printTime(end-start);
-        return 0;
-}
-
-int getstarttime()
-{
-    int starttime;
-    starttime = 1;
-    return starttime;
-}
-
-int getendtime()
-{
-    int endtime;
-    endtime = 32767;
-    return endtime;
-}
-
-int delay()
-{
-   int a = getstarttime();
-   int b = getendtime();
-   int c = a;
-   int d = b;
+   float a = 14.5;
+   float b = 16;
+   float c = 17.5;
+   int x = 3;
+   int y = 6;
+   int z = 5;
+   int i, HCF, max, step, LCM;
+   float sum, prod, divs, mod, modu, cira, cirb;
+   float sub, diff, divi, s, area, cir, rec;
+   printf("\n The values are A = %f, B = %f, C=%f \n", a,b,c );
+   printf(" \n The values of X = %d, Y = %d, Z= %d \n", x,y,z);
    
-   for (c = 1; c <= 32767; c++)
-       for (d = 1; d <= 32767; d++)
-       {}
+   printf("\n The sum of all the numbers");
+   sum = a + b + c;
+   printf(" \n sum = %f \n", sum);
+   
+   printf("\n The difference of B and A ");
+   sub = b - a;
+   printf("\n difference = %f", sub);
+   
+   printf("\n The difference of C and B");
+   diff = c - b;
+   printf("\n difference = %f \n ", diff);
+   
+   printf(" \n The product of all numbers ");
+   prod = a * b * c;
+   printf("\n the product = %f \n", prod);
+   
+   printf("\n The division of B and A ");
+   divs = b/a;
+   printf(" \n The quotient = %f  \n", divs);
+   
+   printf("\n The division of C and B ");
+   divi = c/b;
+   printf("\n The quotient = %f \n", divi); 
+   
+   printf(" \n The even and Odd numbers of X, Y and Z :");
+   if( x%2 == 0)
+   printf ("\n X is an even number which is: %d ", x);
+   else
+   printf("\n X is an odd number which is : %d ", x);
+   
+   if( y%2 == 0)
+   printf ("\n Y is an even number which is: %d", y);
+   else
+   printf("\n Y is an odd number which is : %d", y);
+   
+   if( z%2 == 0)
+   printf ("\n Z is an even number which is: %d",z);
+   else
+   printf("\n Z is an odd number which is : %d \n", z);
+   
+   
+   printf(" \n The greatest of A, B and C");
+   if(a>b && a>c)
+   printf(" \n A is greater which is %f \n", a);
+   else if( b>a && b>c)
+   printf(" \n B is greater which is %f \n", b);
+   else
+   printf("\n C is greater which is %f \n", c);
+   
+   printf(" \n The greatest of X, Y and Z");
+   if(x>y && x>z)
+   printf(" \n X is greater which is %d \n", x);
+   else if( y>x && y>z)
+   printf(" \n Y is greater which is %d \n", y);
+   else
+   printf("\n Z is greater which is %d \n", z);
+  
+   printf("\n The area of triangle A, B and C");
+   s= (sum)/ 2; 
+   area = sqrt(s * (s - a) * (s - b) * (s - c));
+   printf("\n The area = %f \n", area);
+   
+   printf("\n The area of circle C ");
+   cir = 2 * 3.14 * c;
+   printf("\n Area = %f \n", cir);
+   
+   printf("\n The area of circle B ");
+   cirb = 2 * 3.14 * b;
+   printf("\n Area = %f \n", cirb);
+   
+   printf("\n The area of circle A ");
+   cira = 2 * 3.14 * a;
+   printf("\n Area = %f \n", cira);
+   
+   printf( " \n The area of rectangle length A and Breadth C");
+   rec = a * c;
+   printf("\n  Area = %f \n ", rec);
+   
+   printf(" \n To find the HCF and LCM of X,Y and Z");
+   
+   
+   for(i = 1; i <= x || i <= y || i< z ; i++) 
+   {
        
-   return 0;
-}
+   if( x%i == 0 && y%i == 0 && z%i ==0 )
+      HCF = i;
+   }
 
+   printf("\n HCF is = %d \n", HCF);
+   
+   
+   LCM = 0;
 
-void updateTimes()
-{
-        end = time(0);
-}
+   if(x > y && x >z)
+      max = step = x;
+   else if (y> x && y> z )
+      max = step = y;
+   else 
+      max = step = z;
 
-void initTimes()
-{
-        start = time(0);
-        end = start;
-}
+   while(1) 
+   {
+      if(max%x == 0 && max%y == 0 && max%z == 0) 
+      {
+         LCM = max;
+         break;    
+      }
 
-void printPrompt()
-{
-        printf(PROMPT_STRING);
-}
+      max += step;
+   }
 
-void printTime (TIME_TYPE timeInit)
-{
-        TIME_TYPE time = timeInit;
-        int days, hours, minutes, seconds;
-        int timeDifferential = 0;
-        
-        timeDifferential = (SECONDS_PER_MINUTE *
-                                MINUTES_PER_HOUR *
-                                HOURS_PER_DAY);
-        days = (time/timeDifferential);
-        time -= timeDifferential*days;
-        /* Hours */
-        timeDifferential = (SECONDS_PER_MINUTE *
-                                MINUTES_PER_HOUR);
-        hours = (time/timeDifferential);
-        time -= timeDifferential*hours;
-        /* Minutes */
-        timeDifferential = (SECONDS_PER_MINUTE);
-        minutes = (time/timeDifferential);
-        time -= timeDifferential*minutes;
-        /* Seconds */
-        seconds = time;
-        /* Check for validity of times, then print.
-         * Also print error if need be.*/
-        if (!(seconds < 60 && seconds >= 0 &&
-                        minutes < 60 && minutes >= 0 &&
-                        hours < 24 && hours >= 0 )) {
-                printf ("ERROR! TIME FORMAT!\n");
-        }
-        printf ("Total time (s): %u\n %2dd %2dh %2dm %2ds\n", timeInit, 
-                        days, hours, minutes, seconds);
+   printf(" LCM is=  %d \n", LCM);
+  
+  
 }
