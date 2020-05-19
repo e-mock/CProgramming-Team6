6fb2aeb7ffdc9b86218d25618689ef429acc1431
20073046-Onsite <20073046-Onsite@MYTSL00307>
2020-05-18 19:08:08 -0500

    updated C_Unit

--- Vijay/vijay_practice.c
+++ Vijay/vijay_practice.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-int max_value();
+int list[10] = {2,3,4,5,6,1,19,25,4,15};
+int max_value(int);
+int sum(int, int);
+
+int a,b;
 
 int hello_v() {
+
+	int result,result1;
+
    printf("Welcome Team6.!");
-   max_value();
+
+   result = max_value(list[10]);
+   result1 = sum(a,b);
+
+   printf("The results are %d,%d",result,result1);
 
    return 0;
 }
 
 /* function to find the maximum value from a list of number*/
-int max_value()
+int max_value(list[10])
 {
 
-int list[10] = {2,3,4,5,6,1,19,25,4,15};
 int max_value = 0;
 
 for(int j=0;j<10;j++)
@@ -28,4 +38,9 @@ printf("The Maximum value in the list is %d",max_value);
 
 return max_value;
 
+}
+
+int sum(a,b)
+{
+	return a+b;
 }
