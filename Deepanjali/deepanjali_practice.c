1d64e242f3d0d07f816d71977266415d2ff72e7a
deepanjali3047 <65356332+deepanjali3047@users.noreply.github.com>
2020-05-21 04:25:37 -0500

    Basic math operations

--- Deepanjali/deepanjali_practice.c
+++ Deepanjali/deepanjali_practice.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 
 int hello_d() {
-   printf("Hello Deepa.!");
-   
+
+ /* Local variables to hold results*/
+    int tst_add;
+    int tst_sub;
+    int tst_multi;
+    float tst_div;
+    int a = 10, b = 20;
+
+    tst_add = func_add(a, b);
+    tst_sub = func_sub(a, b);
+    tst_multi = func_multi(a, b);
+    tst_div = func_div(a, b);
+
+    printf("Basic mathematics operation Results");
+    printf("Addition : %d", tst_add);
+    printf("Subtraction : %d", tst_sub);
+    printf("Multiplication : %d", tst_multi);
+    printf("Division : %f", tst_div);
+
    return 0;
+}
+
+
+/* Functions declaration*/
+
+/*Addition function*/
+int func_add(p,q)
+{
+    return (p + q);
+}
+
+/*Subtract function : Max value - Min Value*/
+int func_sub(p, q)
+{
+    int x;
+    if (p > q)
+        x = p - q;
+    else
+        x = q - p;
+    return x;
+}
+
+/*Multiplication function*/
+int func_multi(p, q)
+{
+    return (p * q);
+}
+
+/*Division function*/
+float func_div(p, q)
+{
+    return(p / q);
 }
