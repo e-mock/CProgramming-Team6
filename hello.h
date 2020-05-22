107868b5b208b0941b2948a2dad3be848cb54cd6
ElizabethMock <Elizabeth.AngelicaMock@Ltts.com>
2020-05-21 20:47:40 -0500

    deepa test build fix

--- hello.h
+++ hello.h
@@ -2,14 +2,9 @@
 
 #define HELLO_H
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-
-
 
 // define functions here
 int hello_c();
@@ -19,10 +14,12 @@ int hello_v();
 int hello_d();
 
 int add(int a, int b);
-float temp_sensor_converter(int temp_sensor_value, bool* success);
+float temp_sensor_converter(int temp_sensor_value, bool *success);
 float temp_c_to_f(float temp_c);
 bool readings_to_temp_c_and_f_arrays(int *temp_sesnor_readings_ptr, float *temp_c_arr_ptr, float *temp_f__arr_ptr, int number_readings);
 
-
+int func_add(p,q);
+int func_sub(p,q);
+int func_multi(p,q);
 
 #endif
