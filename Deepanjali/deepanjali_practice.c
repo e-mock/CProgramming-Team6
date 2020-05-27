33c11201b8aa6e1dd115a4742efb7a15d71436ca
deepanjali3047 <65356332+deepanjali3047@users.noreply.github.com>
2020-05-26 21:04:36 -0500

    Update deepanjali_practice.c

--- Deepanjali/deepanjali_practice.c
+++ Deepanjali/deepanjali_practice.c
@@ -1,60 +1,70 @@
 #include <stdio.h>
 
-float p,q;
-float a,b;
+float p, q;
+float a, b;
+
 void hello_d() {
 
- /* Local variables to hold results*/
+    /* Local variables to hold results*/
     int tst_add;
     int tst_sub;
     int tst_multi;
     float tst_div;
 
+    printf("Basic mathematics operation Results\n");
     tst_add = func_add(a, b);
     tst_sub = func_sub(a, b);
     tst_multi = func_multi(a, b);
     tst_div = func_div(a, b);
 
-    printf("Basic mathematics operation Results\n");
-    printf("Addition : %d\n", tst_add);
-    printf("Subtraction : %d\n", tst_sub);
-    printf("Multiplication : %d\n", tst_multi);
-    printf("Division : %f\n", tst_div);
-
-   return 0;
+    return 0;
 }
 
 
 /* Functions declaration*/
 
 /*Addition function*/
-int func_add(p,q)
+int func_add(p, q)
 {
-    return (p + q);
+    int add_val, *add_ptr;
+    add_ptr = &add_val;
+    add_val = (p + q);
+    printf("Sum : %d\n", add_val);
+    printf("Address of Sum variable : %p\n", add_ptr);
+    return add_val;
 }
 
 /*Subtract function : Max value - Min Value*/
 int func_sub(p, q)
 {
-    int x;
+    int sub_val, *sub_ptr;
+    sub_ptr = &sub_val;
     if (p > q)
-        x = p - q;
+        sub_val = p - q;
     else
-        x = q - p;
-    return x;
+        sub_val = q - p;
+    printf("Difference : %d\n", sub_val);
+    printf("Address of Diff variable : %p\n", sub_ptr);
+    return sub_val;
 }
 
 /*Multiplication function*/
 int func_multi(p, q)
 {
-    return (p * q);
+    int mul_val, *mul_ptr;
+    mul_ptr = &mul_val;
+    mul_val = (p * q);
+    printf("Multiplied Value : %d\n", mul_val);
+    printf("Address of Multiplied variable : %p\n", mul_ptr);
+    return mul_val;
 }
 
 /*Division function*/
 void func_div(p, q)
 {
-    float y;
-    y = (p / q);
-    return y;
+    float div_val;
+    div_val = (p / q);
+    printf("Division : %f\n", div_val);
+    return div_val;
 
 }
