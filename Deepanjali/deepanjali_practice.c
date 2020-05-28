95a58c46bb742633e92fcac24af0fee1e8905968
deepanjali3047 <65356332+deepanjali3047@users.noreply.github.com>
2020-05-28 11:46:06 -0500

    Update deepanjali_practice.c

--- Deepanjali/deepanjali_practice.c
+++ Deepanjali/deepanjali_practice.c
@@ -103,3 +103,26 @@ int func_date(void) {
   return 0;
 }
  
+int seq_sum() {
+  
+  int i, * ptr, sum = 0;
+  ptr = calloc(20, sizeof(int));
+  
+  if (ptr == NULL) 
+  {
+     printf("Error! memory not allocated.");
+     exit(0);
+  }
+  printf("Building and calculating the sequence sum of the first 10 terms \ n ");
+  
+  for (i = 0; i < 10; ++i) 
+  { 
+    * (ptr + i) = i;
+    sum += * (ptr + i);
+  }
+  
+  printf("Sum = %d", sum);
+  free(ptr);
+  
+  return 0;
+}
