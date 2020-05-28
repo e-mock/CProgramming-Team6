7dae48cdfe9718dd56ba470b5d4ac105de02f75f
deepanjali3047 <65356332+deepanjali3047@users.noreply.github.com>
2020-05-27 22:33:26 -0500

    Function pointer

--- Deepanjali/deepanjali_practice.c
+++ Deepanjali/deepanjali_practice.c
@@ -68,3 +68,21 @@ void func_div(p, q)
     return div_val;
 
 }
+
+/*Function Pointer*/
+int add_array(int* a, int num_elements);
+
+int array_addition() {
+    int Tab[5] = { 100, 220, 37, 16, 98 };
+    printf("Total summation is %d\n", add_array(Tab, 5));
+    return 0;
+}
+
+int add_array(int* p, int size) {
+    int total = 0;
+    int k;
+    for (k = 0; k < size; k++) {
+        total += p[k];  /* it is equivalent to total +=*p ;p++; */
+    }
+    return (total);
+}
