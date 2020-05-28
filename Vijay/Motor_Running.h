9b236b65d3438289c70d3d12f3984970fc6bdcf7
vijayrichard <vijayrichard.4432@gmail.com>
2020-05-28 14:05:10 -0500

    Create Motor_Running.h

---
+++ Vijay/Motor_Running.h
@@ -0,0 +1,19 @@
+/*
+ * Motor_Running.h
+ *
+ *  Created on: May 27, 2020
+ *      Author: 20073046-Onsite
+ */
+
+#ifndef VIJAY_MOTOR_RUNNING_H_
+#define VIJAY_MOTOR_RUNNING_H_
+
+typedef enum
+{
+	M1=0,
+	M2=1
+}MOTOR;
+
+
+
+#endif /* VIJAY_MOTOR_RUNNING_H_ */
