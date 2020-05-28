1c9e2abd9088b304a096307c56082c6108fa8738
vijayrichard <vijayrichard.4432@gmail.com>
2020-05-28 14:03:43 -0500

    Update vijay_practice.c
    merging changes to master

--- Vijay/vijay_practice.c
+++ Vijay/vijay_practice.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include "Motor_Running.h"
 
 int list[10] = {2,3,4,5,6,1,19,25,4,15};/* list to find max value*/
 int max_value();
 int sum(int, int);
+extern int Run_motor(int);
 
 int a,b;
+
+int select = M1;
 
 int hello_v() {
 
@@ -15,10 +19,13 @@ int hello_v() {
    result = max_value();
    result1 = sum(a,b);
 
+   Run_motor(select);
+
    printf("The results are %d,%d",result,result1);
 
    return 0;
 }
+
 
 /* function to find the maximum value from a list of number*/
 int max_value()
@@ -26,7 +33,8 @@ int max_value()
 
 int max_value = 0;
 
-for(int j=0;j<10;j++)
+int j;
+for(j=0;j<10;j++)
 {
 	if(max_value < list[j])
 	{
@@ -34,14 +42,16 @@ for(int j=0;j<10;j++)
 	}
 }
 
-printf("The Maximum value in the list is %d",max_value);
+printf("The Maximum value in the list is :%d",max_value);
 
 return max_value;
 
 }
 
-int sum(a,b)
+int sum(int a,int b)
 {
 	return a+b;
 }
+
+
 
