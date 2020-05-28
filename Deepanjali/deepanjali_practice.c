2c69cb1a012302ce6833a84ddb93afb7afb51512
deepanjali3047 <65356332+deepanjali3047@users.noreply.github.com>
2020-05-28 13:38:17 -0500

    Update deepanjali_practice.c

--- Deepanjali/deepanjali_practice.c
+++ Deepanjali/deepanjali_practice.c
@@ -126,3 +126,35 @@ int seq_sum() {
   
   return 0;
 }
+
+int func_arr_dyn() 
+{
+    int * arr_dynamic = NULL;
+    int elements = 2, i;
+    arr_dynamic = calloc(elements, sizeof(int)); //Array with 2 integer blocks
+	
+    for (i = 0; i < elements; i++)
+	{
+		arr_dynamic[i] = i;
+	}
+    for (i = 0; i < elements; i++) 
+	{
+		printf("arr_dynamic[%d]=%d\n", i, arr_dynamic[i]);
+	}
+	
+    elements = 4;
+    arr_dynamic = realloc(arr_dynamic, elements * sizeof(int)); //reallocate 4 elements
+    printf("After realloc\n");
+	
+    for (i = 2; i < elements; i++)
+	{
+		arr_dynamic[i] = i;
+	}
+	
+    for (i = 0; i < elements; i++)
+	{
+		printf("arr_dynamic[%d]=%d\n", i, arr_dynamic[i]);
+	}
+	
+    free(arr_dynamic);
+}
